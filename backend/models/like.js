b5e81ad0986c25339445f4e58b8f54d849fb2655
sophia pinto <erickfreire616@gmail.com>
2024-12-08 17:51:35 -0300

    atualizando em tempo real

--- backend/models/like.js
+++ backend/models/like.js
@@ -8,6 +8,8 @@ async function toggleLike(PostID, UserID) {
   const checkSql = 'SELECT * FROM Likes WHERE PostID = ? AND UserID = ?';
   const [likes] = await db.execute(checkSql, [PostID, UserID]);
 
+  let liked; 
+  
   if (likes.length > 0) {
     const deleteSql = 'DELETE FROM Likes WHERE PostID = ? AND UserID = ?';
     await db.execute(deleteSql, [PostID, UserID]);
@@ -15,7 +17,7 @@ async function toggleLike(PostID, UserID) {
     const decrementLikeSql = 'UPDATE Posts SET likeCount = likeCount - 1 WHERE PostID = ?';
     await db.execute(decrementLikeSql, [PostID]);
 
-    return { liked: false };
+    liked = false; 
   } else {
     const insertSql = 'INSERT INTO Likes (PostID, UserID) VALUES (?, ?)';
     await db.execute(insertSql, [PostID, UserID]);
@@ -23,13 +25,13 @@ async function toggleLike(PostID, UserID) {
     const incrementLikeSql = 'UPDATE Posts SET likeCount = likeCount + 1 WHERE PostID = ?';
     await db.execute(incrementLikeSql, [PostID]);
 
-    return { liked: true };
+    liked = true; 
   }
 
   const [updatedLikeCount] = await db.execute('SELECT likeCount FROM Posts WHERE PostID = ?', [PostID]);
 
-  return { liked, likeCount: updatedLikeCount[0].likeCount }; // Retorna o estado e a contagem
-
+  return { liked, likeCount: updatedLikeCount[0].likeCount }; 
 }
+
 
 module.exports = { toggleLike };
